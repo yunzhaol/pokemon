0b7495c6cdf5ad8b33c44155ba4a751ddd69c06f
yunzhaol <107370489+yunzhaol@users.noreply.github.com>
2024-03-26 18:37:35 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Yunzhao Li [...UPDATE THIS...]
+# Date: 26 March 2024 [...UPDATE THIS...]
+# Contact: yunzhao.li@mail.utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
