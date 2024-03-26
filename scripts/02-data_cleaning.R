70ef2f43f8a95f3532d285ad85c6183bc4a60c0f
yunzhaol <107370489+yunzhaol@users.noreply.github.com>
2024-03-26 18:38:13 -0400

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Yunzhao Li [...UPDATE THIS...]
+# Date: 26 March 2024 [...UPDATE THIS...]
+# Contact: yunzhao.li@mail.utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
